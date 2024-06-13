18dd5678748ca5f805ed71e868a68121787af02e
lakas <VasilyevNA22@st.ithub.ru>
2024-06-13 12:41:26 +0300

    first commit

---
+++ Create_DB.py
@@ -0,0 +1,14 @@
+import sqlite3 as sq
+
+
+with sq.connect("logsParser.db") as con:
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS logs(
+        h TEXT,
+        l TEXT,
+        u TEXT,
+        t TEXT,
+        r TEXT,
+        s TEXT,
+        b TEXT
+    , UNIQUE(h, t, r))""")
